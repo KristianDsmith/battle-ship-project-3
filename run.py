9a0fcaa367b6f9d4b11274641fb8026b1a9b45c5
KristianDsmith <>
2023-07-03 14:13:34 +0000

    Add code for user to determine the number of hit ships on the game board

--- run.py
+++ run.py
@@ -58,3 +58,12 @@ def get_ship_location():
              print('Not an appropriate choice, please select a valid column')
              column = input("Enter the column of the ship: ").upper()
              return int(row) - 1, letters_to_numbers[column]
+
+# Check if all ships are hit
+def count_hit_ships(board)
+    count = 0
+    for row in board:
+        for column in row:
+            if column == 'X':
+                count += 1
+    return count
