53278864b4668c617d37ca129ebf5d428f3f77fb
KristianDsmith <>
2023-07-03 13:41:28 +0000

    Add game board size in grid format and columns with hiidn board and guess board

--- run.py
+++ run.py
@@ -1,3 +1,27 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+# Rules
+# X fr placing ship and hit battleship
+# ' ' for availble space
+# '-' for missed shot
+
+from random import randint
+
+#board for hlding ship locations
+
+HIDDEN_BOARD = [[" "] * 8 for x in range(8)]
+
+# Board for displaying hits and misses
+
+GUESS_BOARD = [[" "] * 8 for x in range(8)]
+
+# The output of this function would make a game board in a grid format.
+# Row numbers on the left and column labels on the top.
+# The contents of the board are displayed 
+# Using "|" separators between each element.
+
+def print_board(board):
+    print("  A B C D E F G H")
+    print("  +-+-+-+-+-+-+-+")
+    row_number = 1
+    for row in board:
+        print("%d|%s|" % (row_number, "|".join(row)))
+        row_numbers += 1
